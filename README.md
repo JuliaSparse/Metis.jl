5e18109eec0d49c4c2b4ded4820eb4a279a32de8
Douglas Bates <dmbates@gmail.com>
2013-04-30 16:41:44 -0500

    Add the `partGraphKway` and `partGraphRecursive` functions
    Also the `Metis.testgraph` function to read the graph files distributed with Metis.  Update README.md

--- README.md
+++ README.md
@@ -6,10 +6,48 @@
 Pkg.add("Metis")
 ```
 
-## Obtaining a fill-reducing permutation for a sparse matrix
+Adding the package will download, configure and install metis-5.1.0 in the directory
+```julia
+joinpath(Pkg.dir(), "Metis", "deps")
+```
+Configuration requires Cmake version 2.8 or later.  At present the build is only available on Linux/Unix.  Contributions of build stanzas for Windows or OS X are welcome.
+
+## Functions
+
+The names of the available Julia functions are those from the Metis API
+
+`nodeND(al)`
+: create a fill-reducing permutation from the adjacency list, `al`, of a symmetric sparse matrix
+`partGraphKWay(al, nparts::Integer)`
+: partition a graph given as an adjacency list, `al`, into nparts 
+`partGraphRecursive(al, nparts::Integer)`
+: partition a graph given as an adjacency list, `al`, into nparts 
+
+## Examples
+
+The function `Metis.testgraph` can be used to read one of the sample graphs available in the `graphs` directory of `metis-5.1.0`.  These graphs correspond to 2D and 3D finite element meshes.
+
+4elt
+: a smaller sample graph (15606 vertices, 45878 edges)
+copter2 
+: a medium size sample graph (55476 vertices, 352238 edges)
+mdual
+: a larger sample graph (258569 vertices, 513132 edges)
 
 ```julia
 using Graphs, Metis
+copter2 = Metis.testgraph("copter2");
+perm, iperm = nodeND(copter2)
+objval, part = partGraphKWay(copter2, 6)
+counts = zeros(Int, 6);
+for p in part counts[p] += 1 end
+println(counts)
+```
+
+## Obtaining a fill-reducing permutation for a sparse matrix
+
+The function sparse2adjacencylist in the Graphs package creates and adjacency list representation of the pattern of a `SparseMatrixCSC` object.  To be used with nodeND the original sparse matrix must be Hermitian.
+```julia
 ## if A is a sparse Hermitian matrix
 perm, iperm = nodeND(sparse2adjacencylist(A)) 
 ## returns a fill-reducing permutation and its inverse
--- src/Metis.jl
+++ src/Metis.jl
@@ -6,36 +6,74 @@
 module Metis
     using Graphs
 
-    export nodeND
+    export
+        nodeND,
+        partGraphKway,
+        partGraphRecursive
 
     include("metis_h.jl")
 
     const metis_options = -ones(Int32, METIS_NOPTIONS) # defaults indicated by -1
     metis_options[METIS_OPTION_NUMBERING] = int32(1)   # 1-based numbering of vertices
 
-    function nodeND{T<:Integer}(al::GenericAdjacencyList{T,Range1{T},Vector{Vector{T}}})
-        if is_directed(al) error("nodeND applies only to undirected graphs") end
+    ## Create the 1-based CSR representation of the adjacency list
+    function mkadj{T<:Integer}(al::GenericAdjacencyList{T,Range1{T},Vector{Vector{T}}})
+        if is_directed(al) error("Metis functions require undirected graphs") end
         if first(al.vertices) != 1 error("Vertices must be numbered from 1") end
-        n = num_vertices(al)
-        nz = num_edges(al)
+        length(al.adjlist), int32(cumsum(vcat(1, map(length, al.adjlist)))), int32(vcat(al.adjlist...))
+    end
+        
+    function nodeND{T<:Integer}(al::GenericAdjacencyList{T,Range1{T},Vector{Vector{T}}})
+        n, xadj, adjncy = mkadj(al)
         perm = Array(Int32, n)
         iperm = Array(Int32, n)
-        cp = int32(cumsum(vcat(1, map(length, al.adjlist))))
-        rv = Array(Int32, nz)
-        pos = 1
-        for v in al.adjlist, el in v
-            rv[pos] = el
-            pos += 1
-        end
-        try 
-            err = ccall((:METIS_NodeND,:libmetis), Int32,
-                        (Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32},
-                         Ptr{Int32}, Ptr{Int32}),
-                        &int32(n), cp, rv, C_NULL, metis_options, perm, iperm)
-            if (err != METIS_OK) error("METIS_NodeND returned error code $err") end
-        catch
-            error("See http://glaros.dtc.umn.edu/gkhome/views/metis to install libmetis")
-        end
+        err = ccall((:METIS_NodeND,:libmetis), Int32,
+                    (Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32},
+                     Ptr{Int32}, Ptr{Int32}),
+                    &int32(n), xadj, adjncy, C_NULL, metis_options, perm, iperm)
+        if (err != METIS_OK) error("METIS_NodeND returned error code $err") end
         perm, iperm
+    end
+
+    function partGraphKway{T<:Integer}(al::GenericAdjacencyList{T,Range1{T},Vector{Vector{T}}},
+                                       nparts::Integer)
+        n, xadj, adjncy = mkadj(al)
+        part = Array(Int32, n)
+        objval = zeros(Int32, 1)
+        err = ccall((:METIS_PartGraphKway,:libmetis), Int32,
+                    (Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32},
+                     Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32},
+                     Ptr{Int32}, Ptr{Int32}, Ptr{Int32}),
+                    &int32(n), &one(Int32), xadj, adjncy, C_NULL, C_NULL, C_NULL, &int32(nparts),
+                    C_NULL, C_NULL, metis_options, objval, part)
+        if (err != METIS_OK) error("METIS_PartGraphKWay returned error code $err") end
+        objval, part
+    end
+
+    function partGraphRecursive{T<:Integer}(al::GenericAdjacencyList{T,Range1{T},Vector{Vector{T}}},
+                                            nparts::Integer)
+        n, xadj, adjncy = mkadj(al)
+        part = Array(Int32, n)
+        objval = zeros(Int32, 1)
+        err = ccall((:METIS_PartGraphKway,:libmetis), Int32,
+                    (Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32},
+                     Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32},
+                     Ptr{Int32}, Ptr{Int32}, Ptr{Int32}),
+                    &int32(n), &one(Int32), xadj, adjncy, C_NULL, C_NULL, C_NULL, &int32(nparts),
+                    C_NULL, C_NULL, metis_options, objval, part)
+        if (err != METIS_OK) error("METIS_PartGraphKWay returned error code $err") end
+        objval, part
+    end
+
+    function testgraph(nm::ASCIIString)
+        pathnm = joinpath(Pkg.dir(), "Metis", "deps", "metis-5.1.0", "graphs", string(nm, ".graph"))
+        ff = open(pathnm, "r")
+        nvert, nedge = map(int, split(readline(ff)))
+        adjlist = Array(Vector{Int32}, nvert)
+        for i in 1:nvert adjlist[i] = map(int32, split(readline(ff))) end
+        GenericAdjacencyList{Int32,Range1{Int32},Vector{Vector{Int32}}}(false,
+                                                                        one(Int32):int32(nvert),
+                                                                        nedge,
+                                                                        adjlist)
     end
 end
