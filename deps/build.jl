7997e7f8566011aab0c633cc5f78ec921833ee3c
Douglas Bates <dmbates@gmail.com>
2014-04-24 12:35:10 -0500

    More attempts to use the BinDeps package.  Still not working.

--- deps/build.jl
+++ deps/build.jl
@@ -4,25 +4,28 @@ using BinDeps
 
 libmetis = library_dependency("libmetis", aliases=["libmetis5"])
 
-#provides(AptGet,"libmetis5",libmetis)
-#provides(Yum,"metis-5.1.0",libmetis)
+provides(AptGet,"libmetis-dev",libmetis)
+provides(Yum,"metis-5.1.0",libmetis)
+
+@BinDeps.load_dependencies
 
 provides(Sources,
          URI("http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz"),
-         libmetis)
+         libmetis,
+         unpacked_dir="metis-5.1.0")
 
 metisbuilddir = BinDeps.builddir(libmetis)
-println(libmetis)
-println(libdir(libmetis))
-println(metisbuilddir)
+#println(libmetis)
+#println(libdir(libmetis))
+#println(metisbuilddir)
 
 provides(BuildProcess,
 	(@build_steps begin
-            println("In buildsteps")
+#            println("In buildsteps")
 	    GetSources(libmetis)
-	    println("past GetSources")
+#	    println("past GetSources")
 	    CreateDirectory(metisbuilddir)
-            println("past CreateDirectory")
+#            println("past CreateDirectory")
 	    @build_steps begin
 		ChangeDirectory(metisbuilddir)
 	    end
