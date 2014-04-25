b18754198eff87ec74a2b6c3af58e77741840965
Douglas Bates <dmbates@gmail.com>
2014-04-25 14:15:35 -0500

    Add sample graphs and a nodeND function for sparse symmetric matrices

---
+++ deps/deps.jl
@@ -0,0 +1,4 @@
+macro checked_lib(libname, path)
+        (dlopen_e(path) == C_NULL) && error("Unable to load \n\n$libname ($path)\n\nPlease re-run Pkg.build(package), and restart Julia.")
+        quote const $(esc(libname)) = $path end
+    end
