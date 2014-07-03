dbde18a1d67964df54933e95b7130e9dd946e974
Douglas Bates <dmbates@gmail.com>
2014-07-03 15:28:32 -0500

    Added more tests for coverage.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,23 @@ using StatsBase
 
 copter2 = Metis.testgraph("copter2");
 perm, iperm = nodeND(copter2)
+@test all(invperm(perm) .== iperm)
+
+srand(12321)
+a = convert(SparseMatrixCSC{Float64,Int32},sprand(1000,100,0.01))
+ata = a'a
+perm, iperm = nodeND(ata,3)
+@test all(invperm(perm) .== iperm)
+
+perm, iperm = nodeND(ata,0)             # reset the verbosity
+@test all(invperm(perm) .== iperm)
+
 objval, part = partGraphKway(copter2, 6)
 @test counts(part,6) == [9076,9374,9384,9523,8978,9141]
+
+objval, part = partGraphRecursive(copter2,6)
+@test counts(part,6) == [9076,9374,9384,9523,8978,9141]
+
+mdual = Metis.testgraph("mdual")
+objval, part = partGraphKway(mdual, 10)
+@test counts(part,10) == [25789,25731,25790,25998,25728,25724,25722,26061,25995,26031]
