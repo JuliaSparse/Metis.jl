dcbd0195ea3cf56a31a80ee22c24f30424e69fe2
Tony Kelman <tony@kelman.net>
2014-09-02 18:30:20 -0700

    delete deps.jl, autogenerated

--- deps/deps.jl
+++
@@ -1,5 +0,0 @@
-macro checked_lib(libname, path)
-        (dlopen_e(path) == C_NULL) && error("Unable to load \n\n$libname ($path)\n\nPlease re-run Pkg.build(package), and restart Julia.")
-        quote const $(esc(libname)) = $path end
-    end
-@checked_lib libmetis "libmetis"
