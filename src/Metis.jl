e2f42138bf50556929aabe6c92a7fedb69e0bdd4
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-03-28 18:06:43 +0200

    load libmetis from BinaryProvider

--- src/Metis.jl
+++ src/Metis.jl
@@ -1,5 +1,15 @@
+__precompile__(true)
+
 module Metis
 
-# package code goes here
+# Load libmetis with BinaryProvider
+__init__() = check_deps()
+let depsfile = joinpath(@__DIR__, "..", "deps", "deps.jl")
+    if isfile(depsfile)
+        include(depsfile)
+    else
+        error("$(depsfile) does not exist, Please re-run Pkg.build(\"Metis\"), and restart Julia.")
+    end
+end
 
 end # module
