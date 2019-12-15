85c5ce4658547be3eae21e8a668f10db99794d2e
Fredrik Ekre <ekrefredrik@gmail.com>
2019-12-16 00:14:11 +0100

    Replace BinaryProvider with METIS_jll. (#31)

--- src/Metis.jl
+++ src/Metis.jl
@@ -2,18 +2,8 @@ module Metis
 
 using SparseArrays
 using LinearAlgebra
-using Libdl
 import LightGraphs
-
-# Load libmetis with BinaryProvider
-__init__() = check_deps()
-let depsfile = joinpath(@__DIR__, "..", "deps", "deps.jl")
-    if isfile(depsfile)
-        include(depsfile)
-    else
-        error("$(depsfile) does not exist, Please re-run Pkg.build(\"Metis\"), and restart Julia.")
-    end
-end
+using METIS_jll: libmetis
 
 # Metis C API
 include("metis_h.jl")
