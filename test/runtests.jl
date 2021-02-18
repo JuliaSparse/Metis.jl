a7c66f8704f00c9c2e4bfa5e6a6dfe0cb14616ff
Rodolfo Carvajal <rodolfo.carvajal@gmail.com>
2021-02-18 17:57:39 -0300

    Adding a simple test for Metis.partition with edge weights

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Random
 using Test
 using SparseArrays
 import LightGraphs
+import SimpleWeightedGraphs
 
 @testset "Metis.permutation" begin
     Random.seed!(0)
@@ -18,6 +19,21 @@ end
     S = sprand(10, 10, 0.5); S = S + S'; fill(S.nzval, 1)
     T = LightGraphs.smallgraph(:tutte)
     for G in (S, T), alg in (:RECURSIVE, :KWAY), nparts in (3, 4)
+        partition = Metis.partition(G, nparts, alg = alg)
+        @test extrema(partition) == (1, nparts)
+        @test all(x -> findfirst(==(x), partition) !== nothing, 1:nparts)
+    end
+end
+
+@testset "Metis.partition with edge weights" begin
+    T = LightGraphs.smallgraph(:tutte)
+    G = SimpleWeightedGraphs.SimpleWeightedGraph(LightGraphs.nv(T))
+    for i in 1:LightGraphs.nv(T)
+        for j in LightGraphs.neighbors(T, i)
+            SimpleWeightedGraphs.add_edge!(G, i, j, 1)
+        end
+    end
+    for alg in (:RECURSIVE, :KWAY), nparts in (3, 4)
         partition = Metis.partition(G, nparts, alg = alg)
         @test extrema(partition) == (1, nparts)
         @test all(x -> findfirst(==(x), partition) !== nothing, 1:nparts)
