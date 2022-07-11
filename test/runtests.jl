e42fdedc5c61418c86841f1003d181df072c0d3d
Fredrik Ekre <ekrefredrik@gmail.com>
2022-07-12 01:40:28 +0200

    Add support for edge weights from sparse matrix input.
    This patch adds the weights keyword argument to Metis.graph to construct
    a Metis.Graph with edge weights based on the matrix entries, i.e.:
    
        g = Metis.graph(A; weights=true)
    
    Note that the weights must be integers.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,25 @@ using Test
 using SparseArrays
 import LightGraphs, Graphs
 
+@testset "Metis.graph(::SparseMatrixCSC)" begin
+    rng = MersenneTwister(0)
+    S = sprand(rng, Int, 10, 10, 0.2); S = S + S'; fill!(S.nzval, 1)
+    foreach(i -> S[i, i] = 0, 1:10); dropzeros!(S)
+    g = Metis.graph(S)
+    gw = Metis.graph(S; weights=true)
+    @test g.xadj == gw.xadj
+    @test g.adjncy == gw.adjncy
+    @test g.adjwgt == C_NULL
+    @test gw.adjwgt == ones(Int, length(gw.adjncy))
+    G  = SparseMatrixCSC(10, 10, g.xadj, g.adjncy, ones(Int, length(g.adjncy)))
+    GW = SparseMatrixCSC(10, 10, gw.xadj, gw.adjncy, gw.adjwgt)
+    @test iszero(S - G)
+    @test iszero(S - GW)
+end
+
 @testset "Metis.permutation" begin
     rng = MersenneTwister(0)
-    S = sprand(rng, 10, 10, 0.5); S = S + S'; fill(S.nzval, 1)
+    S = sprand(rng, 10, 10, 0.5); S = S + S'; fill!(S.nzval, 1)
     perm, iperm = Metis.permutation(S)
     @test isperm(perm)
     @test isperm(iperm)
@@ -15,10 +31,11 @@ end
 
 @testset "Metis.partition" begin
     rng = MersenneTwister(0)
-    S = sprand(rng, 10, 10, 0.5); S = S + S'; fill(S.nzval, 1)
+    S = sprand(rng, Int, 10, 10, 0.5); S = S + S'; fill!(S.nzval, 1)
+    SG = Metis.graph(S; weights=true)
     T = Graphs.smallgraph(:tutte)
     TL = LightGraphs.smallgraph(:tutte)
-    for G in (S, T, TL), alg in (:RECURSIVE, :KWAY), nparts in (3, 4)
+    for G in (S, T, TL, SG), alg in (:RECURSIVE, :KWAY), nparts in (3, 4)
         partition = Metis.partition(G, nparts, alg = alg)
         @test extrema(partition) == (1, nparts)
         @test all(x -> findfirst(==(x), partition) !== nothing, 1:nparts)
@@ -27,7 +44,8 @@ end
 
 @testset "Metis.separator" begin
     rng = MersenneTwister(0)
-    S = sprand(rng, 10, 10, 0.5); S = S + S'; fill(S.nzval, 1)
+    S = sprand(rng, 10, 10, 0.5); S = S + S'; fill!(S.nzval, 1)
+    SG = Metis.graph(S; weights=true)
     T = Graphs.smallgraph(:tutte)
     TL = LightGraphs.smallgraph(:tutte)
     for G in (S, T, TL)
